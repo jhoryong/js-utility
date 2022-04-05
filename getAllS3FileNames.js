a2ce007acbe7841dee5f8e015791f6ab84f38887
Horyong Jung <jhoryong@logibros.com>
2022-04-05 17:56:39 +0900

    update comment

--- getAllS3FileNames.js
+++ getAllS3FileNames.js
@@ -4,17 +4,21 @@
  * @param {string} baseUrl - ex) https://{bucketName}.s3.ap-northeast-2.amazonaws.com/
  */
 function getAllS3FileNames(baseUrl) {
-    // print all filenames
+    // 1. Get array of file names
     const nodeList = document.querySelectorAll('.name.object.latest.object-name');
-    const fileNameArr = Array.from(nodeList, e => e.textContent); // File name array
+    const fileNameArr = Array.from(nodeList, e => e.textContent);
+    
+    // 2. Print filenames
     let fileText = '';
     fileNameArr.forEach((fileName) => { fileText += fileName + '\n' });
     console.log(fileText);
 
-    // if baseUrl exists, print full url of files    
+    // 3. If baseUrl exists, print full url of files    
     if (baseUrl) {
-        const url = window.location.href;
+        const url = window.location.href; // Get url
         const filePath = url.split('&')[1].split('=')[1]; // Extract file path from url
+        
+        // Concatenate url to filenames and print
         let urlText = '';
         fileNameArr.forEach((fileName) => { urlText += baseUrl + filePath + fileName + '\n' });
         console.log(urlText);
